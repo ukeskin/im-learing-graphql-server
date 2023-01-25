e91d7667d41639e0bf328f6eeedb915f7daf3ae4
Umut Keskin <ukeskin2626@gmail.com>
2023-01-25 16:12:11 +0300

    crud with dummy data

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server listening => http://localhost:${port}
+  `);
+});
---
+++ server/sampleData.js
@@ -0,0 +1,46 @@
+const projects = [
+  {
+    id: "1",
+    clientId: "1",
+    name: "E Commerce Website",
+    description: "Build an e-commerce website for a client",
+    status: "In Progress",
+  },
+  {
+    id: "2",
+    clientId: "2",
+    name: "Mobile App",
+    description: "Build a mobile app for a client",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    clientId: "3",
+    name: "Web App",
+    description: "Build a web app for a client",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "John Doe",
+    email: "johndoe@gmail.com",
+  },
+  {
+    id: "2",
+    name: "Jane Doe",
+    email: "asd@gmail.com",
+  },
+  {
+    id: "3",
+    name: "Jane 2Doe",
+    email: "asd@gmail.com",
+  },
+];
+
+module.exports = {
+  projects,
+  clients,
+};
